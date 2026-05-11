2d5f71ae12f87a7f3a297ee9e61fc55bd487eaac
PacomeDutreuil <13007@holbertonstudents.com>
2026-05-11 12:00:21 +0200

    Add Task 2

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module qui définit une classe Square."""
+
+
+class Square:
+    """Classe représentant un carré."""
+
+    def __init__(self, size):
+        """Initialise un carré avec une taille privée."""
+        self.__size = size
