ced15b1fbe020fb00edec652f85b119224fecedd
PacomeDutreuil <13007@holbertonstudents.com>
2026-05-11 12:06:12 +0200

    Add Task 4

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module qui définit une classe Square."""
+
+
+class Square:
+    """Classe représentant un carré."""
+
+    def __init__(self, size=0):
+        """Initialise un carré avec validation de la taille."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Retourne l'aire du carré."""
+        return self.__size ** 2
